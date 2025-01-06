abc16a2422f284557787519c48a2d819e52f0ae0
jinzequn <jinzequn@foxmail.com>
2025-01-06 10:03:10 +0800

    keep MultibindingIterateKey & multibinding elements definition order across modules

--- projects/core/koin-core/src/commonMain/kotlin/org/koin/core/registry/InstanceRegistry.kt
+++ projects/core/koin-core/src/commonMain/kotlin/org/koin/core/registry/InstanceRegistry.kt
@@ -15,6 +15,7 @@
  */
 package org.koin.core.registry
 
+import co.touchlab.stately.concurrency.AtomicInt
 import org.koin.core.Koin
 import org.koin.core.annotation.KoinExperimentalAPI
 import org.koin.core.annotation.KoinInternalApi
@@ -26,6 +27,7 @@ import org.koin.core.definition.indexKey
 import org.koin.core.instance.ResolutionContext
 import org.koin.core.instance.InstanceFactory
 import org.koin.core.instance.NoClass
+import org.koin.core.instance.OrderedInstanceFactory
 import org.koin.core.instance.ScopedInstanceFactory
 import org.koin.core.instance.SingleInstanceFactory
 import org.koin.core.module.Module
@@ -42,6 +44,7 @@ import kotlin.reflect.KClass
 @Suppress("UNCHECKED_CAST")
 @OptIn(KoinInternalApi::class)
 class InstanceRegistry(val _koin: Koin) {
+    private val instanceOrder = AtomicInt(0)
 
     private val _instances = safeHashMap<IndexKey, InstanceFactory<*>>()
     val instances: Map<IndexKey, InstanceFactory<*>>
@@ -94,7 +97,11 @@ class InstanceRegistry(val _koin: Koin) {
             }
         }
         _koin.logger.debug("(+) index '$mapping' -> '${factory.beanDefinition}'")
-        _instances[mapping] = factory
+        _instances[mapping] = if (factory is OrderedInstanceFactory<*>) {
+            factory.copy(order = instanceOrder.incrementAndGet())
+        } else {
+            factory
+        }
     }
 
     private fun createEagerInstances(instances: Collection<SingleInstanceFactory<*>>) {
@@ -168,7 +175,8 @@ class InstanceRegistry(val _koin: Koin) {
         allowOverride: Boolean = true,
     ) {
         val rootQualifier = _koin.scopeRegistry.rootScope.scopeQualifier
-        val def = _createDefinition(Kind.Scoped, qualifier, { instance }, secondaryTypes, rootQualifier)
+        val def =
+            _createDefinition(Kind.Scoped, qualifier, { instance }, secondaryTypes, rootQualifier)
         val factory = SingleInstanceFactory(def)
         val indexKey = indexKey(def.primaryType, def.qualifier, def.scopeQualifier)
         saveMapping(allowOverride, indexKey, factory)
@@ -180,7 +188,8 @@ class InstanceRegistry(val _koin: Koin) {
 
     internal fun dropScopeInstances(scope: Scope) {
         val factories = _instances.values.toTypedArray()
-        factories.filterIsInstance<ScopedInstanceFactory<*>>().forEach { factory -> factory.drop(scope) }
+        factories.filterIsInstance<ScopedInstanceFactory<*>>()
+            .forEach { factory -> factory.drop(scope) }
     }
 
     internal fun close() {
@@ -196,7 +205,17 @@ class InstanceRegistry(val _koin: Koin) {
                 (factory.beanDefinition.primaryType == clazz || factory.beanDefinition.secondaryTypes.contains(clazz))
             }
             .distinct()
+            .sortedIfNecessary()
             .mapNotNull { it.get(instanceContext) as? T }
+    }
+
+    private fun List<InstanceFactory<*>>.sortedIfNecessary(): List<InstanceFactory<*>> {
+        return if (all { it is OrderedInstanceFactory<*> }) {
+            this as List<OrderedInstanceFactory<*>>
+            this.sorted()
+        } else {
+            this
+        }
     }
 
     internal fun unloadModules(modules: Set<Module>) {
